1bc0a099ec793747ea06f06dd2a1eae63523cb2b
Karan Sikka <karanssikka@gmail.com>
2025-03-18 15:34:16 -0400

    bbox transform for video inference (#13)

--- lp3d_analysis/io.py
+++ lp3d_analysis/io.py
@@ -1,3 +1,6 @@
+import re
+from pathlib import Path
+
 import yaml
 from omegaconf import DictConfig
 
@@ -32,4 +35,21 @@ def load_lightning_pose_cfg(config_path: str):
     return DictConfig(lightning_pose_cfg)
 
 
+def collect_files_by_token(files: list[Path], tokens: list[str]) -> dict[str, list[Path]]:
+    """Given a list of files, collects them by presence of token in their filenames.
 
+    Token must separated by the rest of the filename by some non-alphanumeric delimiter.
+    E.g. for token "top", mouse_top_3.mp4 is allowed, but mousetop3.mp4 is not allowed."""
+    files_by_token: dict[str, list[Path]] = {}
+    for token in tokens:
+        # Search all the video_files for a match.
+        for file in files:
+            if re.search(rf"(?<!0-9a-zA-Z){re.escape(token)}(?![0-9a-zA-Z])", file.stem):
+                if token not in files_by_token:
+                    files_by_token[token] = []
+                files_by_token[token].append(file)
+        # After the search if nothing was added to dict, there is a problem.
+        if token not in files_by_token:
+            raise ValueError(f"File not found for token: {token}")
+
+    return files_by_token
